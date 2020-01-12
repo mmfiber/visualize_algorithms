4badf7b9019c41b6440f9a4fd53d71b888b9afed
mmfiber <masa.03191996@gmail.com>
2020-01-12 17:53:22 +0900

    redirect

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -8,7 +8,8 @@ export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    { path: '/', redirect: 'sort' },
+    { path: '/', name: 'home', redirect: 'sort' },
     { path: '/sort', name: 'sort', component: Sort },
+    { path: '*', redirect: 'home' }
   ]
 })
